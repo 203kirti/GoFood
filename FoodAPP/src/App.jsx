f9b817661f160ecad420a09d836c1026fbc57c05
Kirti <kirti.air203@gmail.com>
2024-11-14 01:25:21 +0530

    Created Navbar , login , Home page REACT

---
+++ FoodAPP/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router , Routes, Route , Link} from "react-router-dom"
+import { useState } from 'react'
+import Home from './screens/Home';
+import Login from "./screens/Login";
+
+function App() {
+  
+  return (
+    <Router>
+      <div >
+
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+
+      </div>
+    </Router>
+  );
+}
+
+export default App
---
+++ FoodAPP/src/screens/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+function Home() {
+  return (
+    <>
+    <div> <Navbar/> 
+    </div>
+    <div>Body</div>
+    <div>
+        <Footer />
+    </div>
+    </>
+  )
+}
+
+export default Home
